5880a3c7e59d7d32fc4ed035e9c04ad385aadcdc
bart <hyperbart@protonmail.com>
2022-10-09 08:16:20 +1300

    MVP working

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./sqlite-pass-4/db.sqlite3");
+
+db.serialize(() => {
+  console.log("ready");
+});
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { query } = req.body;
+  db.all(
+    `SELECT * FROM transactions_search where username_address match ?;`,
+    query,
+    (err, transactions) => {
+      console.log("err", err);
+      console.log("transactions", transactions);
+      res.status(200).json({ success: true, transactions });
+    }
+  );
 }
