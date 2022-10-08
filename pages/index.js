9104ca098f623d273411fb18ba99c88c37d8b1f6
bart <hyperbart@protonmail.com>
2022-10-09 08:22:08 +1300

    improve layout

--- pages/index.js
+++ pages/index.js
@@ -40,10 +40,22 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Search Celsius Transactions</h1>
 
+        <p className={styles.description}>
+          Easily look up transactions from{" "}
+          <a
+            href="https://gizmodo.com/celsius-execs-cashed-out-bitcoin-price-crypto-ponzi-1849623526"
+            rel="noopener noreferrer"
+          >
+            Celsius bankruptcy proceedings
+          </a>
+          .
+        </p>
+
         <form className={styles.description} onSubmit={submit}>
           <input
             className={styles.inputbox}
-            type="text"
+            name="q"
+            type="search"
             placeholder="Username or address"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
@@ -54,53 +66,15 @@ export default function Home() {
 
         <pre>{JSON.stringify(transactions, null, 2)}</pre>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+        <a href="https://twitter.com/0xHyperbart" rel="noopener noreferrer">
+          Twitter: 0xHyperbart
         </a>
+        <div class={styles.small}>
+          More features coming soon &mdash; watch this space.
+        </div>
       </footer>
     </div>
   );
